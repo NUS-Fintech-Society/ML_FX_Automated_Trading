7aa6f89f2c4a236b6655c2a9c03c06991ffbd84c
linhuaian <65528303+linhuaian@users.noreply.github.com>
2021-09-12 22:20:08 +0800

    Update scrapper_template.py

--- Scrappers/scrapper_template.py
+++ Scrappers/scrapper_template.py
@@ -2,11 +2,14 @@ import requests
 
 url = 'https://e6hx5erhc6.execute-api.ap-southeast-1.amazonaws.com/Fintech/fintech_data_pipe'
 
+##
 def scrapper_template():
-    body = {
-      "values": {
-        "test3": 999
-      }
-    }
+    body = scrap_data()
+    print(body)
     resp = requests.post(url, json = body)
     print(resp.text)
+
+def scrap_data():
+    ##Scrapping function
+    return {"values": {"interest_cn":0.02, "interest_aus":0.03}} ##example output format
+
