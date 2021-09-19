9bb9382df2de7f90f034bf96b70c38c7d24737ea
linhuaian <65528303+linhuaian@users.noreply.github.com>
2021-09-19 14:46:59 +0800

    Edited HP imports

--- Scrappers/scrapper_hp.py
+++ Scrappers/scrapper_hp.py
@@ -9,7 +9,7 @@ import requests
 import datetime
 from oandapyV20 import API
 import oandapyV20.endpoints.pricing as pricing
-from constants_hp import POSITIVE_CORRELATION, YAHOO_OPTION, YAHOO_STOCK, EPOCH_WEEK, VOLAFY_URL, IVR_LST
+from constants_hp import POSITIVE_CORRELATION, YAHOO_OPTION, YAHOO_STOCK, EPOCH_WEEK, VOLAFY_URL, IVR_LST, ACCOUNT_ID
 
 
 url = 'https://e6hx5erhc6.execute-api.ap-southeast-1.amazonaws.com/Fintech/fintech_data_pipe'
@@ -125,5 +125,5 @@ def scrape_main():
 
     resp = requests.post(url, json = body)
 
-# positive_correlation_query_oanda()
-# scrape_main()
+#positive_correlation_query_oanda()
+scrape_main()
