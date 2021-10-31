0269d8e166064fbefc24567262fa4a73005e72e2
linhuaian <65528303+linhuaian@users.noreply.github.com>
2021-10-31 23:23:02 +0800

    Removed matplotlib

--- Execution/LogisticRegression.py
+++ Execution/LogisticRegression.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import requests
 import json
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
